4178edb795e3d8fd51d8c5728e7f426a674f5880
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-08-09 05:57:48 -0400

    Create event listeners to display password paragraphs when input is focused, and hide when input is unfocused

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const inputPwd1 = document.querySelector('#pwd1');
 
+const reqPwdPara = document.querySelectorAll('.pwd-req')
+
+
+
+
+inputPwd1.addEventListener('focus', () => {
+    for (pwd of reqPwdPara) {
+        pwd.style.display = "block"; 
+    }
+})
+
+inputPwd1.addEventListener('focusout', () => {
+    for (pwd of reqPwdPara) {
+        pwd.style.display = "none"
+    }
+})
+
+
+
+// Adds back paragraphs without proper line breaks, also need to return to display: none when input is not focused
