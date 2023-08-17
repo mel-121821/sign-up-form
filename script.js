35d6fbcb8d9ef957acd3e4e8a82858ae5e4b9578
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-08-17 06:27:15 -0400

    Add event listeners to show email para when focused, also add an event listener to add/remove a class to email span when its input is valid/invalid

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const inputPwd1 = document.querySelector('#pwd1');
 const inputPwd2 = document.querySelector('#pwd2');
 
 const emailPara = document.querySelector('.email-req')
+const emailParaSpan = document.querySelector('.email-req > span')
+
 const reqPwdPara = document.querySelectorAll('.pwd-req');
 const pwdMatch = document.querySelector('.pwd-match');
 
@@ -15,6 +17,22 @@ inputEmail.addEventListener('focus', () => {
 inputEmail.addEventListener('focusout', () => {
     emailPara.style.display = "none";
 })
+
+
+
+inputEmail.addEventListener('input', (event)=> {
+    if(inputEmail.validity.valid) {
+        emailParaSpan.classList.add('email-valid'); 
+        console.log("Valid");
+    } else {
+        emailParaSpan.classList.remove('email-valid')
+        console.log("Not valid")
+    }
+    
+})
+
+
+
 
 inputPwd1.addEventListener('focus', () => {
     for (pwd of reqPwdPara) {
